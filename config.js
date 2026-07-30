3c0c251cd3fe402f70a0af3ae998fc164effa0a8
Abhay Verma <100143863+abhayvermacse@users.noreply.github.com>
2026-07-30 18:44:35 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
-// Rename this file to 'config.js' and replace 'YOUR_TMDB_API_KEY' with your actual key
+//  replace 'YOUR_TMDB_API_KEY' with your actual key
 const TMDB_API_KEY = 'YOUR_TMDB_API_KEY';
